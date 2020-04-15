28481883135b1b820a9b8910c46a31f02c9fcf75
cmzhong <cheriemzhong@gmail.com>
2020-04-14 22:25:56 -0700

    webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: 
+       [
+          {
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            use: 
+            {
+              loader: 'babel-loader',
+              options: 
+              {
+                // @ sign is important!!
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // order matters here..
+              // you need style and css before you can access sass
+              'style-loader',
+              'css-loader',
+              'sass-loader',
+            ],
+          },
+          { test: /\.css$/, use: 'css-loader' }
+        ]
+      },
+    devServer: {
+      publicPath: '/build/',
+      proxy: {
+        // optional to place stars /api/**
+        '/api': 'http://localhost:3000'
+      }
+    },
+ // so you don't have to place .js or .jsx at the end of your import/export files   
+    resolve: {
+      extensions: [".js", ".jsx"]
+    }
+// for windows users to read process.env
+    // plugins: [
+    //   new webpack.DefinePlugin({
+    //     'process.env' : {
+    //       NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    //     }
+    //   })
+    // ]
+};
